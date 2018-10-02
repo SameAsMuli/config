5f227852b10f65cf9e28c500a2fbea54582bf68d
dm1438 <dm1438@saamis1.daiad.dai.co.uk>
2018-10-02 11:42:51 +0100

    Reshuffle structure

---
+++ setup.sh
@@ -0,0 +1,35 @@
+CUSTOM_CONFIG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CUSTOM_REFS_DIR="$CUSTOM_CONFIG_DIR/.refs"
+
+if [ ! -d $CUSTOM_REFS_DIR ]
+then
+  mkdir -p $CUSTOM_REFS_DIR
+fi
+
+CUSTOM_CONFIG_COMMANDS_DIR=$CUSTOM_CONFIG_DIR/commands
+if [ -d $CUSTOM_CONFIG_COMMANDS_DIR ]
+then
+  for file in $(find $CUSTOM_CONFIG_COMMANDS_DIR -type f -not -name "*.md" -not -path '*/\.*')
+  do
+    source $file
+  done
+else
+  unset CUSTOM_CONFIG_COMMANDS_DIR
+fi
+
+CUSTOM_CONFIG_PACKAGES_DIR=$CUSTOM_CONFIG_DIR/packages
+if [ ! -d $CUSTOM_CONFIG_PACKAGES_DIR ]
+then
+  unset CUSTOM_CONFIG_PACKAGES_DIR
+fi
+
+CUSTOM_CONFIG_TERMINAL_DIR=$CUSTOM_CONFIG_DIR/terminal
+if [ -d $CUSTOM_CONFIG_TERMINAL_DIR ]
+then
+  for file in $(find $CUSTOM_CONFIG_TERMINAL_DIR -type f -not -name "*.md" -not -path '*/\.*')
+  do
+    source $file
+  done
+else
+  unset CUSTOM_CONFIG_TERMINAL_DIR
+fi
