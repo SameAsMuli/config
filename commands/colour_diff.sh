1f8899d2a1813403509a9600233f5835cc825f2c
dm1438 <dm1438@saamis1.daiad.dai.co.uk>
2018-10-24 16:58:04 +0100

    Use less for colour_diff outputs greater than screen length

--- commands/colour_diff.sh
+++ commands/colour_diff.sh
@@ -19,5 +19,5 @@ function diff()
     return 1
   fi
 
-  command diff -u ${ARGS[0]} ${ARGS[1]} | sed '0,/\(--- .*\)/s//\x1b[1;31m\1/;0,/\(+++ .*\)/s//\x1b[1;32m\1/;s/^-/\x1b[31m-/;s/^+/\x1b[32m+/;s/^@/\x1b[35m@/;s/$/\x1b[0m/'
+  command diff -u ${ARGS[0]} ${ARGS[1]} | sed '0,/\(--- .*\)/s//\x1b[1;31m\1/;0,/\(+++ .*\)/s//\x1b[1;32m\1/;s/^-/\x1b[31m-/;s/^+/\x1b[32m+/;s/^@/\x1b[35m@/;s/$/\x1b[0m/' | less -FRSX
 }
