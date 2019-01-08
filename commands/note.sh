b8dcb936eb1af7e0be837c0e67ade1591045ac28
Sam Amis <30879203+SameAsMuli@users.noreply.github.com>
2019-01-08 12:31:05 +0000

    Add quick note files changes from vim

--- commands/note.sh
+++ commands/note.sh
@@ -1,19 +1,26 @@
-function note() {
+if [ -z "$CUSTOM_REFS_DIR" ]
+then
   if [ -z "$CUSTOM_CONFIG_DIR" ]
   then
-    echo "Error: Variable CUSTOM_CONFIG_DIR not defined..."
-    return 1
-  elif [ -z "$CUSTOM_REFS_DIR" ]
-  then
+    CUSTOM_REFS_DIR=$HOME
+  else
     CUSTOM_REFS_DIR=$CUSTOM_CONFIG_DIR
   fi
+fi
 
-  if [ ! -d "$CUSTOM_REFS_DIR" ]
+if [ ! -d "$CUSTOM_REFS_DIR" ]
+then
+  mkdir -p $CUSTOM_REFS_DIR
+fi
+
+export NOTE_FILE=$CUSTOM_REFS_DIR/note
+
+function note() {
+  if [ -z $NOTE_FILE ]
   then
-    mkdir -p $CUSTOM_REFS_DIR
+    echo "Error: Variable NOTE_FILE not defined..."
+    return 1
   fi
-
-  export NOTE_FILE=$CUSTOM_REFS_DIR/.note
 
   # Decide the editor to use
   if [ -z "$EDITOR" ]
@@ -30,19 +37,19 @@ function note() {
     echo "note [--list] [filename]"
   elif [[ $1 == "--list" ]]
   then
-    if [[ `ls $CUSTOM_REFS_DIR/.note.* 2> /dev/null | wc -l` == 0 ]]
+    if [[ `ls $NOTE_FILE-* 2> /dev/null | wc -l` == 0 ]]
     then
       echo "No additional notes files"
     else
       echo "Existing notes files:"
-      ls $CUSTOM_REFS_DIR/.note.* | sed 's/.*\./  /'
+      ls $NOTE_FILE-* | sed 's/.*-/  /'
     fi
   elif [[ $1 =~ "-" ]]
   then
     echo "Unknown option: $1"
     return 1
   else
-    $EDITOR $NOTE_FILE.$1
+    $EDITOR $NOTE_FILE-$1
   fi
 }
 
