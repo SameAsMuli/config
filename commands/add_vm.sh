2550647bc34ca6682bfdd6738f43c775aab3844e
Sam Amis <s_amis@hotmail.co.uk>
2019-10-14 13:27:02 +0100

    Fix add_vm script to be more portable

--- commands/add_vm.sh
+++ commands/add_vm.sh
@@ -1,10 +1,10 @@
 # Enable passwordless ssh to another vm
 add_vm ()
 {
-  if [ $# -neq 2 ]
+  if [ "$#" != "1" ]
   then
     echo "Usage: add_vm <user>@<hostname>"
-    exit 1
+    return 1
   fi
 
   cat ~/.ssh/id_rsa.pub | ssh $1 'if [ ! -f ~/.ssh/authorized_keys ]; then mkdir -p ~/.ssh; touch ~/.ssh/authorized_keys; fi; cat >> ~/.ssh/authorized_keys; chmod 644 ~/.ssh/authorized_keys'
