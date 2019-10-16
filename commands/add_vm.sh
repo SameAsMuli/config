8cb65085d65e692775b0fd8c22176776a1ae2a0c
Sam Amis <s_amis@hotmail.co.uk>
2019-10-16 17:46:52 +0100

    Make add_vm command more robust

--- commands/add_vm.sh
+++ commands/add_vm.sh
@@ -7,5 +7,17 @@ add_vm ()
     return 1
   fi
 
+  if [ ! -f ~/.ssh/id_rsa.pub ] || [ ! -s ~/.ssh/id_rsa.pub ]
+  then
+    echo "Public ssh key does not exist... Generating now:"
+    ssh-keygen -t rsa
+
+    if [ ! -f ~/.ssh/id_rsa.pub ] || [ ! -s ~/.ssh/id_rsa.pub ]
+    then
+      echo "Failed to generate public key"
+      return 1
+    fi
+  fi
+
   cat ~/.ssh/id_rsa.pub | ssh $1 'if [ ! -f ~/.ssh/authorized_keys ]; then mkdir -p ~/.ssh; touch ~/.ssh/authorized_keys; fi; cat >> ~/.ssh/authorized_keys; chmod 644 ~/.ssh/authorized_keys'
 }
